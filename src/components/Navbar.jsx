086243b40ec13f600cdfe169641f120c58c25ac7
euricodande12 <euricodande12@gmail.com>
2024-06-24 20:23:18 +0200

    SignIn and SignUp pages created, Some Authentication code added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const navbar = () => {
   return (
-    <nav className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <h1 className='capitalize text-red-600 text-4xl font-bold cursor-pointer'>Netflix</h1>
-        <div>
-            <button className='text-white pr-4'>Sign In</button>
-            <button className='bg-red-600 hover:bg-red-700 transition-all px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
-        </div>
+    <nav className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <Link to="/">
+        <h1 className="capitalize text-red-600 text-4xl font-bold cursor-pointer">
+          Netflix
+        </h1>
+      </Link>
+      <div>
+        <Link to='/login'>
+        <button className="text-white pr-4">Sign In</button>
+        </Link>
+        <Link to='/signup'>
+        <button className="bg-red-600 hover:bg-red-700 transition-all px-6 py-2 rounded cursor-pointer text-white">
+          Sign Up
+        </button>
+        </Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import bgImage from '../img/bg-dark.jpg'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <main class='w-full h-screen'>
+        <img src={bgImage} alt="Netflix Background" className='hidden sm:block absolute w-full h-full object-cover' />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className='fixed w-full px-4 py-24 z-50'>
+            <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
+                <div className='max-w-[320px] mx-auto py-16'>
+                    <h1 className='text-3xl font-bold'>Sign In</h1>
+                    <form className="w-full flex flex-col py-4">
+                        <input type="email" placeholder='Email' className="p-3 my-2 bg-gray-600 rounded" autoComplete="email" />
+                        <input type="password" placeholder='Password' className="p-3 my-2 bg-gray-600 rounded" autoComplete='current-password' />
+                        <button className="bg-red-600 py-3 my-6 rounded font-bold">Sign In</button>
+                        <div className='flex justify-between items-center text-sm text-gray-600'>
+                            <p><input type="checkbox" className='mr-2' /> Remember me</p>
+                            <p className='cursor-pointer'>Need Help?</p>
+                        </div>
+                        <p className='py-8'><span className='text-gray-600'>New to Netflix?</span>{' '}
+                        <Link to='/signup'>
+                        Sign Up
+                        </Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default Login
